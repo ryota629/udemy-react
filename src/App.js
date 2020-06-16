9d1a55ae08e0eb04516d1183cca2d1112a061554
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2020-06-16 18:46:05 +0900

    create commit compoent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React,　{ Component } from 'react';
+import React, { Component } from 'react';
 
-class App extends Component {
-  render(){
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked")}} />
-      </div>
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked")}} />
+//       </React.Fragment>
         
-  // const greeting = "Hi,Tom!";
-  // const dom = <h1 className="foo">{greeting}</h1>;
-  // return dom;
-    )
-  }
+//   // const greetbing = "Hi,Tom!";
+//   // const dom = <h1 className="foo">{greeting}</h1>;
+//   // return dom;
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
 export default App;
