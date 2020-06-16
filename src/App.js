05a8015f131d9d9c3b6e33c259b9fbcb4867ea0a
ryota629 <ryo721.tddc_bluer@ezweb.ne.jp>
2020-06-16 17:51:48 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
-
+import React,　{ Component } from 'react';
 
 class App extends Component {
   render(){
-    return <div><h1>Hello,world!!</h1></div>
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}} />
+      </div>
+        
+  // const greeting = "Hi,Tom!";
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
+    )
   }
 }
 export default App;
